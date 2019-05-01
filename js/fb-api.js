7fbcbedd202b66d051fb581e9f82f8bca1f58d00
warbodi4 <warbodi4@gmail.com>
2019-05-01 21:05:53 +0300

    new

--- js/fb-api.js
+++ js/fb-api.js
@@ -8,7 +8,7 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
-      window.location = "https://warbodi4.github.io/project.html";
+      window.location.replace("https://warbodi4.github.io/project.html");
     } else {
       // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
