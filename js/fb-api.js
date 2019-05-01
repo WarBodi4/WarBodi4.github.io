e6b2930d81412415de617507d643bd6b6f2d2bab
warbodi4 <warbodi4@gmail.com>
2019-05-01 21:01:41 +0300

    new

--- js/fb-api.js
+++ js/fb-api.js
@@ -8,6 +8,7 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
+      window.location.href = "http://warbodi4.github.io/project.html";
     } else {
       // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
@@ -63,6 +64,6 @@ function statusChangeCallback(response) {
   }
   
   
-  if (testAPI()) {
-    window.location.href = "http://warbodi4.github.io/project.html";
-  }
+  
+    
+  
