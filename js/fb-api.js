0cc0693127d6890cf5eef2ee3f8be116f7e7aa59
WarBodi4 <49321527+WarBodi4@users.noreply.github.com>
2019-05-01 22:49:54 +0300

    Update fb-api.js

--- js/fb-api.js
+++ js/fb-api.js
@@ -1,25 +1,19 @@
 function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
+    
     if (response.status === 'connected') {
-      // Logged into your app and Facebook.
+     
       testAPI();
         
 
     } else {
-      // The person is not logged into your app or we are unable to tell.
+      
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
     }
   }
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
@@ -30,10 +24,9 @@ function statusChangeCallback(response) {
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '144117349792063',
-      cookie     : true,  // enable cookies to allow the server to access 
-                          // the session
-      xfbml      : true,  // parse social plugins on this page
-      version    : 'v3.3', // The Graph API version to use for the call
+      cookie     : true,               
+      xfbml      : true,  
+      version    : 'v3.3',
      
     });
 
@@ -53,8 +46,7 @@ function statusChangeCallback(response) {
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
+  
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
