28d5b883630a4a0df892a4a3dddc0b4b79474b8e
WarBodi4 <49321527+WarBodi4@users.noreply.github.com>
2019-05-01 23:07:29 +0300

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -6,7 +6,7 @@
             //Створення світла
             var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, 100), scene);
         
-            //Adding an Arc Rotate Camera
+            //Створення камери
             var camera = new BABYLON.ArcRotateCamera("Camera", 1.3, 1, 15, BABYLON.Vector3.Zero(), scene);
             camera.attachControl(canvas, false);
 
@@ -22,8 +22,7 @@
                 // Встановлення камери за першим об'єктом
                 camera.target = newMeshes[0];
 
-   //cylinder1.rotate(BABYLON.Axis.Z, -1, BABYLON.Space.WORLD);
-   // dverkaholod.rotate(BABYLON.Axis.Y, -2, BABYLON.Space.WORLD);
+  
 
             });
  
@@ -48,7 +47,7 @@
 
 
 
-        // Resize
+        // Зміна розміру 
         window.addEventListener("resize", function () {
             engine.resize();
         });
@@ -58,12 +57,12 @@
       //Функція для відкритя і закриття дверки холодилька
         function holod() {
     var n = document.getElementById("open-close");
-    if (n.value == "Open"){
-        n.value  = "Close";
+    if (n.value == "Закрити"){
+        n.value  = "Відкрити";
         dverkaholod.rotate(BABYLON.Axis.Y,9.7,BABYLON.Space.WORLD);
     }
     else{
-        n.value  = "Open";
+        n.value  = "Закрити";
         dverkaholod.rotate(BABYLON.Axis.Y, -9.7, BABYLON.Space.WORLD);
     }
 
@@ -72,21 +71,12 @@
 //Функція для відкритя і закриття дверки морозильної камери
 function moroz() {
     var el = document.getElementById("opeclose");
-    if (el.value === "Open"){
-        el.value  = "Close";
+    if (el.value === "Закрити"){
+        el.value  = "Відкрити";
         dverkamoroz.rotate(BABYLON.Axis.Y,9.7,BABYLON.Space.WORLD);
     }
     else{
-        el.value  = "Open";
+        el.value  = "Закрити";
         dverkamoroz.rotate(BABYLON.Axis.Y, -9.7, BABYLON.Space.WORLD);
     }
-}
-
-window.addEventListener("click", function (evt) {
-   // We try to pick an object
-   var pickResult = scene.pick(evt.clientX, evt.clientY);
-});
-        if (pickResult.hit) {
-            impact.position.x = pickResult.pickedPoint.x;
-            impact.position.y = pickResult.pickedPoint.y;
 }
